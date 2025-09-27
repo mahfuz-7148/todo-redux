bc7ce536003e2f04a4f5afdf7c56755e86fd8283
mahfuz-7148 <mrahman7148@gmail.com>
2025-09-27 21:16:30 +0600

    added todo completed

--- src/components/todoFilter.jsx
+++ src/components/todoFilter.jsx
@@ -12,6 +12,7 @@ export const TodoFilter = ({stats, currentFilter, OnFilter}) => {
     <div className='flex items-center justify-center'>
       <div className='inline-flex bg-gray-200 rounded-lg p-1'>
         {
+
           filters.map(({key, label, icon: Icon, count}) => {
             return (
               <button onClick={() => OnFilter(key)} className={`flex items-center gap-2 px-2 py-2 rounded-md text-sm font-medium transition-all duration-200 ${currentFilter === key ? 'bg-white text-gray-800 shadow-md' : 'text-gray-700 hover:text-gray-800 hover: bg-gray-300'}`} key={key}>
